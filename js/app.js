edcb2a4c0cf7ff4fe0a0b92c45f9a48fb4c19627
Alberto-Zapardiel-Fernandez <alberto.zapardiel.fernandez@gmail.com>
2023-01-28 20:48:44 +0100

    boton de empezar el juego para darle funcionalidad

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 import { getPokemons } from './API/Calls.js';
+import { goToSelection } from './Functions/functions.js';
 /* Limit of Pokemons */
 const LIMIT = 151;
 /* URL to search ⬆️ pokemons */
 const URL_ALL_POKEMONS = `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=${LIMIT}`;
 document.addEventListener('DOMContentLoaded', getPokemons(URL_ALL_POKEMONS));
+
+const btnStart = document.getElementById('btnStart');
+btnStart.addEventListener('click', goToSelection);
