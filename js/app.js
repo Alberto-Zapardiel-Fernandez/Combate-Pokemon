b4e9ec2c96d5370b7ec0c5c8990ab34522106b54
Alberto-Zapardiel-Fernandez <alberto.zapardiel.fernandez@gmail.com>
2023-01-31 13:07:56 +0100

    export del numero limite y cambio en nombre

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 import { getPokemons } from './API/Calls.js';
-import { goToSelection } from './Functions/functions.js';
+import { goToCombat } from './Functions/functions.js';
 /* Limit of Pokemons */
-const LIMIT = 151;
+export const LIMIT = 151;
 /* URL to search ⬆️ pokemons */
 const URL_ALL_POKEMONS = `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=${LIMIT}`;
 document.addEventListener('DOMContentLoaded', getPokemons(URL_ALL_POKEMONS));
 
 const btnStart = document.getElementById('btnStart');
-btnStart.addEventListener('click', goToSelection);
+btnStart.addEventListener('click', goToCombat);
