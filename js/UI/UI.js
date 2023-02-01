ae9a23a5faf8c04d96d0c2211dd1687c821271cc
Alberto-Zapardiel-Fernandez <alberto.zapardiel.fernandez@gmail.com>
2023-02-01 02:42:14 +0100

    Agregada función para pintar el resultado

--- js/UI/UI.js
+++ js/UI/UI.js
@@ -1,9 +1,4 @@
-import {
-  getPokemon,
-  getPokemonCombat,
-  pokemonsCombatSelected,
-  pokemonsCombatRandom,
-} from '../API/Calls.js';
+import { getPokemon, getPokemonCombat } from '../API/Calls.js';
 import { capitalizeFirstLetter } from '../Functions/functions.js';
 const selectPokemons = document.getElementsByClassName('selectPokemons');
 
@@ -12,9 +7,7 @@ const selectPokemons = document.getElementsByClassName('selectPokemons');
 export function paintPokemonsOptions(pokemons) {
   pokemons.map((pokemon) => {
     for (let i = 0; i < selectPokemons.length; i++) {
-      selectPokemons[
-        i
-      ].innerHTML += `<option id="${pokemon.id}" value="${pokemon.name}">${pokemon.id} - ${pokemon.name}</option>`;
+      selectPokemons[i].innerHTML += `<option id="${pokemon.id}" value="${pokemon.name}">${pokemon.id} - ${pokemon.name}</option>`;
       selectPokemons[i].addEventListener('change', getPokemon);
     }
   });
@@ -23,8 +16,7 @@ export function paintPokemonsOptions(pokemons) {
 /* Function for paint the pokemon name,types and image on html */
 export function paintPokemon(imgSrc, name, abilities, types, e) {
   const id = e.target.id.slice(e.target.id.length - 1);
-  document.getElementById(`pokemonName${id}`).textContent =
-    capitalizeFirstLetter(name);
+  document.getElementById(`pokemonName${id}`).textContent = capitalizeFirstLetter(name);
   document.getElementById(`imgPokemon${id}`).src = imgSrc;
   const typeParagraph = document.getElementById(`typeParagraph${id}`);
   typeParagraph.textContent = 'Tipo: ';
@@ -57,6 +49,9 @@ export function startGame(pokemonsSelected, pokemonsRandom) {
   pokemonsRandom.forEach((pokemon) => getPokemonCombat(pokemon));
 }
 
+export function paintCombatResult(messages) {
+  console.log(messages);
+}
 /* Functions */
 
 function showCombatUI() {
@@ -69,8 +64,7 @@ function paintTypes(types, typeParagraph) {
   if (types.length > 1) {
     types.forEach((type, i) => {
       i < types.length - 1
-        ? (typeParagraph.textContent +=
-            capitalizeFirstLetter(type.type.name) + '/')
+        ? (typeParagraph.textContent += capitalizeFirstLetter(type.type.name) + '/')
         : (typeParagraph.textContent += capitalizeFirstLetter(type.type.name));
     });
   } else {
