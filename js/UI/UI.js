28fcab32d632038227e8b7c65677e2b76fa2b508
Alberto-Zapardiel-Fernandez <alberto.zapardiel.fernandez@gmail.com>
2023-02-03 01:52:07 +0100

    Añadidas funciones de combate final y comentarios

--- js/UI/UI.js
+++ js/UI/UI.js
@@ -1,6 +1,9 @@
 import { getPokemon, getPokemonCombat } from '../API/Calls.js';
 import { capitalizeFirstLetter, playMusic } from '../Functions/functions.js';
-const selectPokemons = document.getElementsByClassName('selectPokemons');
+const selectPokemons = document.getElementsByClassName('selectPokemons'),
+  finalMessages = document.getElementById('finalMessages'),
+  cardSelected = document.getElementById('cardSelected'),
+  cardRandom = document.getElementById('cardRandom');
 
 /* For Each pokemon write an option into select */
 
@@ -49,21 +52,74 @@ export function startGame(pokemonsSelected, pokemonsRandom) {
   pokemonsRandom.forEach((pokemon) => getPokemonCombat(pokemon));
 }
 
+/* Function for panit all results */
 export function paintCombatResult(messages, images, namesPCS, namesPCR) {
   playMusic();
-  const principal = document.querySelector('h1');
-  messages.forEach((message) => {
-    principal.textContent += message;
+  paintImagesPokemonsSelected(images, namesPCS);
+  paintImagesPokemonsRandom(images, namesPCR);
+  showMessages(messages, finalMessages);
+  showPokemonsImages();
+}
+
+/* Functions */
+/* En el box final pone las imágenes de cada pokemon y su nombre de nuestro equipo */
+function paintImagesPokemonsSelected(images, namesPCS) {
+  const imagesPokemons = document.querySelectorAll('.card-selected img'),
+    namesPokemons = document.querySelectorAll('.card-selected p');
+  let i = 0;
+  imagesPokemons.forEach((img) => {
+    img.src = images[i];
+    img.alt = images[i];
+    i++;
+  });
+  i = 0;
+  namesPokemons.forEach((name) => {
+    name.textContent = namesPCS[i];
+    i++;
   });
 }
-/* Functions */
 
+/* En el box final pone las imágenes de cada pokemon y su nombre del equipo rival */
+function paintImagesPokemonsRandom(images, namesPCR) {
+  const imagesPokemons = document.querySelectorAll('.card-random img'),
+    namesPokemons = document.querySelectorAll('.card-random p');
+  let i = 3;
+  imagesPokemons.forEach((img) => {
+    img.src = images[i];
+    img.alt = images[i];
+    i++;
+  });
+  i = 0;
+  namesPokemons.forEach((name) => {
+    name.textContent = namesPCR[i];
+    i++;
+  });
+}
+
+/* Muestra los boxes de imágenes */
+function showPokemonsImages() {
+  cardSelected.classList.remove('card-selected');
+  cardSelected.classList.add('left-to-mid');
+  cardRandom.classList.remove('card-random');
+  cardRandom.classList.add('right-to-mid');
+}
+/* Muestra el box final con los mensajes y su resultado */
+function showMessages(messages, finalMessages) {
+  messages.forEach((message) => {
+    const p = document.createElement('p');
+    p.textContent = message;
+    finalMessages.classList.add('top-to-mid');
+    finalMessages.appendChild(p);
+  });
+}
+/* Cambia la vista a la final ocultando las actuales */
 function showCombatUI() {
   document.getElementById('empezar').classList.add('d-none');
   document.getElementById('seleccion').classList.add('d-none');
   document.getElementById('combate').classList.remove('d-none');
 }
 
+/* Dibuja los tipos de cada pokemon */
 function paintTypes(types, typeParagraph) {
   if (types.length > 1) {
     types.forEach((type, i) => {
@@ -75,13 +131,13 @@ function paintTypes(types, typeParagraph) {
     typeParagraph.textContent += capitalizeFirstLetter(types[0].type.name);
   }
 }
-
+/* Muestra las habilidades de cada pokemon */
 function paintAbilities(abilitiesList, abilities) {
   abilitiesList.textContent = 'Habilidades';
   abilities.forEach((ability) => {
     const li = document.createElement('li');
     li.textContent = capitalizeFirstLetter(ability.ability.name);
-    li.className = 'text-white ';
+    li.className = 'text-warning ';
     abilitiesList.appendChild(li);
   });
 }
