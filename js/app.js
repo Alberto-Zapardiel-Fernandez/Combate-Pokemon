cff20b4bb41d183e064bd808c8eabdbd1f9ffb91
Alberto-Zapardiel-Fernandez <alberto.zapardiel.fernandez@gmail.com>
2023-02-03 01:49:59 +0100

    Añadido comentario

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@ import { getPokemons } from './API/Calls.js';
 import { goToCombat } from './Functions/functions.js';
 /* Limit of Pokemons */
 export const LIMIT = 151;
-/* URL to search ⬆️ pokemons */
+/* URL to search  ⬇️ pokemons */
 const URL_ALL_POKEMONS = `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=${LIMIT}`;
 document.addEventListener('DOMContentLoaded', getPokemons(URL_ALL_POKEMONS));
 
+/* Botón que inicia el combate */
 const btnStart = document.getElementById('btnStart');
 btnStart.addEventListener('click', goToCombat);
